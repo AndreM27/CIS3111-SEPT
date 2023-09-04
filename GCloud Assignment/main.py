d679fac878cf343738a8015e8db267a3fe8a5859
AndreM27 <63149136+AndreM27@users.noreply.github.com>
2023-09-04 20:44:17 +0200

    Add files via upload

--- GCloud Assignment/main.py
+++ GCloud Assignment/main.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import random
 import socket
+from google.cloud.sql.connector import Connector
+import sqlalchemy
 gce_name = socket.gethostname()
 app = Flask(__name__)
 
-
+# initialize parameters
+INSTANCE_CONNECTION_NAME = "cis3111-2023-september:europe-west1:db-instance" # i.e demo-project:us-central1:demo-instance
+print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
+DB_USER = "user"
+DB_PASS = "pass"
+DB_NAME = "pass"
+    
 def generate_random_numbers(count):
     return [random.randint(0, 100000) for _ in range(count)]
     
@@ -18,14 +26,23 @@ def find_smallest(numbers):
     finMIN = "instance "+gce_name+" generated the smallest number :"+str(smallest)
     return finMIN
 
-@app.route('/get_largest_smallest', methods=['GET'])    
+@app.route('/get_largest_smallest', methods=['GET'])  
 def get_largest_smallest():
     random_numbers = generate_random_numbers(15000)
     #lastRet = find_largest_and_smallest(random_numbers)
     #return jsonify({lastRet})
     largest = find_largest(random_numbers)
     smallest = find_smallest(random_numbers)
-    return jsonify({'largest': largest, 'smallest': smallest, 'instance': gce_name, 'numbers generated': len(random_numbers)})
+    numberslen = "numbers generated: "+str(len(random_numbers))
+    return value(largest,smallest,numberslen)
+
+@app.route('/')
+def home():
+    return render_template("/index.html")
+  
+@app.route('/<large>/<small>/<leng>')   
+def value(large, small, leng):
+    return render_template("/index.html", large=large, small=small, leng=leng)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
